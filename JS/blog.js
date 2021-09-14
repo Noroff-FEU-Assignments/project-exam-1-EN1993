8136926b59c78e9ece87e71a72528c86f4ef1a1f
EN1993 <74547465+EN1993@users.noreply.github.com>
2021-09-14 11:20:42 +0200

    add

--- JS/blog.js
+++ JS/blog.js
@@ -5,11 +5,10 @@ async function getBlog(url) {
   const response = await fetch(url);
   const blog = await response.json();
   blog.forEach(function(blogs){
-    console.log(blog);
     blogContainer.innerHTML += `
     <a "href=bloSpecific.html?id=${blogs.id}">
       <div class='blog--posts'>
-        <div class='blog--posts-image'style="background-image:url('${blog.content[0].object.rendered}}')"></div>
+        <div class='blog--posts-image'style="background-image:url('${blogs[0].object.content}')"></div>
 
       </div>
 
---
+++ JS/contact.js
@@ -0,0 +1,69 @@
+
+const form = document.querySelector("#contactForm")
+
+const fName = document.querySelector("#fname");
+const fNameError = document.querySelector(".fNameError");
+
+const lName = document.querySelector("#lname");
+const lNameError = document.querySelector(".lNameError");
+
+const email = document.querySelector("#email");
+const emailError = document.querySelector(".emailError");
+
+const subject = document.querySelector("#subject");
+const subjectError = document.querySelector(".subjectError");
+
+const message = document.querySelector("#textarea");
+const messageError = document.querySelector(".messageError");
+
+
+function validateForm(event) {
+  event.preventDefault();
+
+  if (checkLength(fName.value, 5) === true) {
+    fNameError.style.display ="none";
+  }else{
+    fNameError.style.display ="block";
+  }
+
+  if (checkLength(lName.value, 5) === true) {
+    lNameError.style.display ="none";
+  }else{
+    lNameError.style.display ="block";
+  }
+
+  if (validateEmail(email.value) === true) {
+    emailError.style.display ="none";
+  }else{
+    emailError.style.display ="block";
+  }
+
+  if (checkLength(subject.value, 15) === true) {
+    subjectError.style.display ="none";
+  }else{
+    subjectError.style.display ="block";
+  }
+
+  if (checkLength(message.value, 25) === true) {
+    messageError.style.display ="none";
+  }else{
+    messageError.style.display ="block";
+  }
+
+}
+
+form.addEventListener("submit", validateForm);
+
+function checkLength(value, len) {
+  if (value.trim().length > len) {
+    return true;
+  }else{
+    return false;
+  }
+}
+
+function validateEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const patternMatches = regEx.test(email);
+  return patternMatches;
+}
