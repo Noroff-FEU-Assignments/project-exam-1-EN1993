9b401bff56f8536bf80a766f6035328b2f95b8cb
EN1993 <74547465+EN1993@users.noreply.github.com>
2021-09-15 20:04:17 +0200

    add

--- JS/blog.js
+++ JS/blog.js
@@ -4,17 +4,29 @@ const blogContainer = document.querySelector(".blog");
 async function getBlog(url) {
   const response = await fetch(url);
   const blog = await response.json();
-  console.log(blog);
   blog.forEach(function(blogs){
     blogContainer.innerHTML += `
-    <a "href=bloSpecific.html?id=${blogs.id}">
-      <div class='blog--posts'>
-        <div class='blog--posts-image'style="background-image:url('${blogs.object[0].content.object}')"></div>
-
+    
+      <div class='blog'>
+        <div class='blog--posts-image'style="background-image:url('${blogs._embedded["wp:featuredmedia"]["0"].source_url}')"></div>
+        <span class='featuredmedia-text'><h4>${blogs._embedded["wp:featuredmedia"]["0"].title.rendered}</h4></span>
+        
       </div>
-
-    </a>
     `
   });
 }
 getBlog(baseUrl);
+
+
+const viewMoreTitle = document.querySelector(".blog-post-specific-title");
+
+const viewMoreContainer = document.querySelector(".blog-post-specific");
+
+const viewMoreButton = document.querySelector(".view-more");
+
+function viewMoreSection() {
+  viewMoreTitle.style.display ="flex";
+  viewMoreContainer.style.display ="flex";
+}
+
+viewMoreButton.onclick = viewMoreSection;
