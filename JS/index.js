456c94561df7cf28d500f112e6e68c7021bc3076
EN1993 <74547465+EN1993@users.noreply.github.com>
2021-09-20 13:10:07 +0200

    add

--- JS/index.js
+++ JS/index.js
@@ -1,28 +1,57 @@
 
-const galleryOne = document.querySelector(".one");
+const baseUrl ='https://pe.enlive.one/wp-json/wp/v2/posts?_embed';
+const latestPostContainer = document.querySelector(".galleryshow");
 
-const galleryTwo = document.querySelector(".two");
-
-const galleryThree = document.querySelector(".three");
-
-const prevButton = document.querySelector(".prev");
-
-const nextButton = document.querySelector(".next");
-
-function prevChange() {
+async function latestPost(url) {
+  const response = await fetch(url);
+  const posts = await response.json();
+  console.log(posts);
   
-  galleryOne.style.display ="flex"
-  galleryTwo.style.display ="none"
+    for (let i = 0; i < posts.length; i++) {
+      if (!posts[i]._embedded["wp:featuredmedia"]["0"].categories_names["6"]["0"].name) {
+        continue
+      }
+      
+      latestPostContainer.innerHTML += `
+       <div class='gallery one'>
+
+          <div class='gallery-picture'style="background-image:url('${posts[i]._embedded["wp:featuredmedia"]["0"].source_url}')"></div>
+          <span class='latest-post-text'><h3>${posts[i]._embedded["wp:featuredmedia"]["0"].title.rendered}</h3></span>
+        </div>
+            `
+    }
+
+  
 }
+latestPost(baseUrl);
 
-prevButton.onclick = prevChange;
 
 
-function nextChange() {
+
+// const galleryOne = document.querySelector(".one");
+
+// const galleryTwo = document.querySelector(".two");
+
+// const galleryThree = document.querySelector(".three");
+
+// const prevButton = document.querySelector(".prev");
+
+// const nextButton = document.querySelector(".next");
+
+// function prevChange() {
   
-  galleryTwo.style.display ="flex"
-  galleryOne.style.display ="none"
-  
-}
+//   galleryOne.style.display ="flex"
+//   galleryTwo.style.display ="none"
+// }
 
-nextButton.onclick = nextChange;
+// prevButton.onclick = prevChange;
+
+
+// function nextChange() {
+  
+//   galleryTwo.style.display ="flex"
+//   galleryOne.style.display ="none"
+  
+// }
+
+// nextButton.onclick = nextChange;
