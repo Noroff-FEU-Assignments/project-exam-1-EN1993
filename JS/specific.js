2e2a0251506221150367c99a62afe56ee8904178
EN1993 <74547465+EN1993@users.noreply.github.com>
2021-09-21 14:29:57 +0200

    Update specific.js

--- JS/specific.js
+++ JS/specific.js
@@ -4,22 +4,22 @@ const parms = new URLSearchParams(querystring);
 const id = parms.get("id");
 
 
-const url = "https://pe.enlive.one/wp-json/wp/v2/posts?_embed/" +id;
+const url = "https://pe.enlive.one/wp-json/wp/v2/posts";
 
 const blogSpecificContainer = document.querySelector(".blog-specific");
 
 async function blogSpecific() {
   const response = await fetch(url);
   const specific = await response.json();
-
+    console.log(specific);
   for (let i = 0; i < specific.length; i++) {
     
     blogSpecificContainer.innerHTML += `
   
     <div>
         
-      <div class='blog--specific-image'style="background-image:url('${specific[i]._embedded["wp:featuredmedia"]["0"].source_url}')"></div>
-      <span class='featuredmedia-specific-text'><h4>${specific[i]._embedded["wp:featuredmedia"]["0"].title.rendered}</h4></span> 
+      <div class='blog--specific-image'style="background-image:url('${specific[i].blog_post_layout_featured_media_urls.full[0]}')"></div>
+      <span class='featuredmedia-specific-text'><h4>${specific[i]["wp:featuredmedia"]["0"].title.rendered}</h4></span> 
         
     </div>
   
